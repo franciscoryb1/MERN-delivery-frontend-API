a0e8c689dc7d529afa530ad8182de1f646f5c0af
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-02-05 18:31:53 -0300

    ADD formData al enviar la funcion obSubmit. FIX couisines por cuisines y FIX isLoading.

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -4,11 +4,11 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
 import { Separator } from "@radix-ui/react-separator";
-import CousinesSection from "./CousinesSection";
 import MenuSection from "./MenuSection";
 import ImageSection from "./ImageSection";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import CuisinesSection from "./CousinesSection";
 
 const formSchema = z.object({
     restaurantName: z.string({
@@ -28,7 +28,7 @@ const formSchema = z.object({
         required_error: "Estimated delivery time is required",
         invalid_type_error: "Must be a valid number",
     }),
-    cousines: z.array(z.string()).nonempty({
+    cuisines: z.array(z.string()).nonempty({
         message: "Please select al least one item",
     }),
     menuItems: z.array(
@@ -45,37 +45,54 @@ type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
     onSave: (restaurantFormData: FormData) => void;
-    islLoading: boolean;
+    isLoading: boolean;
 }
 
-const ManageRestaurantForm = ({ onSave, islLoading }: Props) => {
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
     const form = useForm<RestaurantFormData>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            cousines: [],
-            menuItems: [{ name: "", price: 0 }],
-        }
-    });
+          restaurantName: "", // Evita que sea undefined
+          city: "",
+          country: "",
+          deliveryPrice: 0, // Si es numérico, mejor usar 0
+          estimatedDeliveryTime: 0, // Igual aquí
+          cuisines: [],
+          menuItems: [{ name: "", price: 0 }],
+        },
+      });
+      
 
     // Cuando el codigo del form onSubmit llega aca es porque ya paso las validaciones de zod
     const onSubmit = (formDataJson: RestaurantFormData) => {
-        // convert formDataJson to FormData object
         const formData = new FormData();
 
         formData.append("restaurantName", formDataJson.restaurantName);
         formData.append("city", formDataJson.city);
         formData.append("country", formDataJson.country);
-        // Por convencion general se envia el precio en la menor denominacion de la moneda (centavos)
-        formData.append("deliveryPrice", (formDataJson.deliveryPrice * 100).toString());
-        formData.append("estimatedDeliveryTime", formDataJson.estimatedDeliveryTime.toString());
-        formDataJson.cousines.forEach((cousine, index) => {
-            formData.append(`cousines[${index}]`, cousine)
+
+        formData.append(
+            "deliveryPrice",
+            (formDataJson.deliveryPrice * 100).toString()
+        );
+        formData.append(
+            "estimatedDeliveryTime",
+            formDataJson.estimatedDeliveryTime.toString()
+        );
+        formDataJson.cuisines.forEach((cuisine, index) => {
+            formData.append(`cuisines[${index}]`, cuisine);
         });
         formDataJson.menuItems.forEach((menuItem, index) => {
             formData.append(`menuItems[${index}][name]`, menuItem.name);
-            formData.append(`menuItems[${index}][price]`, (menuItem.price * 100).toString());
+            formData.append(
+                `menuItems[${index}][price]`,
+                (menuItem.price * 100).toString()
+            );
         });
-        formData.append("imageFile", formDataJson.imageFile);
+
+        if (formDataJson.imageFile) {
+            formData.append(`imageFile`, formDataJson.imageFile);
+        }
 
         onSave(formData);
     };
@@ -86,13 +103,13 @@ const ManageRestaurantForm = ({ onSave, islLoading }: Props) => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
                 <DetailsSection />
                 <Separator />
-                <CousinesSection />
+                <CuisinesSection />
                 <Separator />
                 <MenuSection />
                 <Separator />
                 <ImageSection />
 
-                {islLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+                {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
 
             </form>
         </Form>
