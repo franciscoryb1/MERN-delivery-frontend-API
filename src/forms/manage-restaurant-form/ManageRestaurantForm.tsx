7f109ff075b576aaec4019d69f8c68f313e73b52
franciscoryb1 <franciscoryb1@hotmail.com>
2025-02-04 23:02:30 -0300

    FIX Incluyo en ManageRestaurantForm: CousineSection y MenuSection.

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@radix-ui/react-separator";
+import CousinesSection from "./CousinesSection";
+import MenuSection from "./MenuSection";
 
 const formSchema = z.object({
     restaurantName: z.string({
@@ -59,8 +62,12 @@ const ManageRestaurantForm = ({ onSave, islLoading }: Props) => {
     return (
         <Form {...form}>
             {/* handleSubmit pasa las validaciones de zod, si todo esta bien se ejecuta el onSubmit */}
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg"> 
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
                 <DetailsSection />
+                <Separator />
+                <CousinesSection />
+                <Separator />
+                <MenuSection />
             </form>
         </Form>
     )
