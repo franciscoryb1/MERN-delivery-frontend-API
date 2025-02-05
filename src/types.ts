1c1eda3114bf303e4cb757bc3f0fa82f561900c0
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-02-05 18:29:07 -0300

    ADD Restaurant type en types.ts

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,23 @@ export type User = {
     city: string;
     country: string;
 };
+
+export type Restaurant = {
+    _id: string;
+    user: string;
+    restaurantName: string;
+    city: string;
+    country: string;
+    deliveryPrice: number;
+    estimatedDeliveryTime: number;
+    cuisines: string[];
+    menu: MenuItem[];
+    imageUrl: string;
+    lastUpdated: string;
+};
+
+export type MenuItem = {
+    _id: string;
+    name: string;
+    price: number;
+};
