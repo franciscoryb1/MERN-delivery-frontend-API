5f3f7645afb8c8b1a94faeed6350bc2c4d5871e4
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-02-05 18:29:23 -0300

    ADD MyRestaurantApi.ts

---
+++ src/api/MyRestaurantApi.tsx
@@ -0,0 +1,60 @@
+import { Restaurant } from "@/types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useCreateMyRestaurant = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    // con Promise<Restaurant> ayudo a que cuando se use el componente se sepa que tipo de dato va a retornar
+    const createMyRestaurantRequest = async (restaurantFormData: FormData): Promise<Restaurant> => {
+        const accessToken = await getAccessTokenSilently();
+        // console.log(accessToken);
+
+        const formDataObj: Record<string, any> = {};
+        restaurantFormData.forEach((value, key) => {
+            formDataObj[key] = value;
+        });
+
+        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            body: restaurantFormData
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            // throw new Error(errorData.message);
+            throw new Error(errorData.message);
+        }
+
+        return response.json();
+
+    };
+
+    const {
+        mutate: createRestaurant,
+        isLoading,
+        isSuccess,
+        error
+    } = useMutation(createMyRestaurantRequest);
+
+    if (isSuccess) {
+        toast.success('Restaurant created');
+    }
+
+    if (error instanceof Error) {
+        // Ahora TypeScript sabe que 'error' es una instancia de Error, por lo que tiene la propiedad 'message'
+        toast.error(error.message || 'Failed to create restaurant');
+    }
+
+    return {
+        createRestaurant,
+        isLoading
+    };
+
+};
