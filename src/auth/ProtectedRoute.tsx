fe88b59aa9dde506bd9bb89cb18293d4f983ff21
franciscoryb1 <franciscoryb1@hotmail.com>
2025-02-05 22:28:37 -0300

    FIX error que al recargar redirije al index. SOLUCIONADO

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -2,10 +2,20 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0();
 
-    // Outlet renderiza el componente que esta dentro de la ruta
-    return isAuthenticated ? (<Outlet /> ) : (<Navigate to="/" replace />)
+    
+    if (isLoading) {
+        return null;
+    }
+    
+    if (isAuthenticated) {
+        // Outlet renderiza el componente que esta dentro de la ruta
+        return <Outlet />;
+    }
+
+    return <Navigate to="/" replace />; // replace para que no se pueda volver a la pagina anterior
+
 };
 
 export default ProtectedRoute;
