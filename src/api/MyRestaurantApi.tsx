181bc524da32fe4603b3785db181d8cb81aa0a91
franciscoryb1 <franciscoryb1@hotmail.com>
2025-02-05 22:18:46 -0300

    ADD useGetMyRestaurant en MyRestaurantApi.tsx

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -1,9 +1,47 @@
 import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// Siempre que se use GET se usa useQuery
+// Siempre que se use POST o PUT se usa useMutation
+
+export const useGetMyRestaurant = () => {
+
+    const { getAccessTokenSilently } = useAuth0();
+
+    const getMyRestaurantRequest = async (): Promise<Restaurant> => {
+        const accessToken = await getAccessTokenSilently();
+
+        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+            headers: {
+                method: 'GET',
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch restaurant data');
+        }
+
+        return response.json();
+    };
+
+    const { data: restaurant, isLoading } = useQuery(
+        'fetchMyRestaurant',
+        getMyRestaurantRequest
+    );
+
+    return {
+        restaurant,
+        isLoading
+    };
+
+};
+
+
 
 export const useCreateMyRestaurant = () => {
     const { getAccessTokenSilently } = useAuth0();
@@ -33,7 +71,6 @@ export const useCreateMyRestaurant = () => {
         }
 
         return response.json();
-
     };
 
     const {
