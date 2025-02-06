cc721b4e72b6783cd65491dd3522309ab63d9d36
franciscoryb1 <franciscoryb1@hotmail.com>
2025-02-05 22:19:29 -0300

    FIX errores en ManageRetaurantForm.tsx y ManageRestaurantPage.tsx

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,118 +3,142 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CousinesSection";
 import MenuSection from "./MenuSection";
 import ImageSection from "./ImageSection";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
-import CuisinesSection from "./CousinesSection";
+import { Restaurant } from "@/types";
+import { useEffect } from "react";
 
-const formSchema = z.object({
+const formSchema = z
+  .object({
     restaurantName: z.string({
-        required_error: "Restaurant name is required",
+      required_error: "restuarant name is required",
     }),
     city: z.string({
-        required_error: "Restaurant city is required",
+      required_error: "city is required",
     }),
     country: z.string({
-        required_error: "Country is required",
+      required_error: "country is required",
     }),
     deliveryPrice: z.coerce.number({
-        required_error: "Delivery price is required",
-        invalid_type_error: "Must be a valid number",
+      required_error: "delivery price is required",
+      invalid_type_error: "must be a valid number",
     }),
     estimatedDeliveryTime: z.coerce.number({
-        required_error: "Estimated delivery time is required",
-        invalid_type_error: "Must be a valid number",
+      required_error: "estimated delivery time is required",
+      invalid_type_error: "must be a valid number",
     }),
     cuisines: z.array(z.string()).nonempty({
-        message: "Please select al least one item",
+      message: "please select at least one item",
     }),
     menuItems: z.array(
-        z.object({
-            name: z.string().min(1, "name is required"),
-            price: z.coerce.number().min(1, "Price is required"),
-        })
+      z.object({
+        name: z.string().min(1, "name is required"),
+        price: z.coerce.number().min(1, "price is required"),
+      })
     ),
-    imageFile: z.instanceof(File, { message: "Image is required" })
-});
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: "Either image URL or image File must be provided",
+    path: ["imageFile"],
+  });
 
 type RestaurantFormData = z.infer<typeof formSchema>;
 
-
 type Props = {
-    onSave: (restaurantFormData: FormData) => void;
-    isLoading: boolean;
-}
+  restaurant?: Restaurant;
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
 
-const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
-    const form = useForm<RestaurantFormData>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          restaurantName: "", // Evita que sea undefined
-          city: "",
-          country: "",
-          deliveryPrice: 0, // Si es numérico, mejor usar 0
-          estimatedDeliveryTime: 0, // Igual aquí
-          cuisines: [],
-          menuItems: [{ name: "", price: 0 }],
-        },
-      });
-      
+const ManageRestaurantForm = ({ onSave, isLoading, restaurant }: Props) => {
+  const form = useForm<RestaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
 
-    // Cuando el codigo del form onSubmit llega aca es porque ya paso las validaciones de zod
-    const onSubmit = (formDataJson: RestaurantFormData) => {
-        const formData = new FormData();
+  useEffect(() => {
+    if (!restaurant) {
+      return;
+    }
 
-        formData.append("restaurantName", formDataJson.restaurantName);
-        formData.append("city", formDataJson.city);
-        formData.append("country", formDataJson.country);
+    // price lowest domination of 100 = 100pence == 1GBP
+    const deliveryPriceFormatted = parseInt(
+      (restaurant.deliveryPrice / 100).toFixed(2)
+    );
 
-        formData.append(
-            "deliveryPrice",
-            (formDataJson.deliveryPrice * 100).toString()
-        );
-        formData.append(
-            "estimatedDeliveryTime",
-            formDataJson.estimatedDeliveryTime.toString()
-        );
-        formDataJson.cuisines.forEach((cuisine, index) => {
-            formData.append(`cuisines[${index}]`, cuisine);
-        });
-        formDataJson.menuItems.forEach((menuItem, index) => {
-            formData.append(`menuItems[${index}][name]`, menuItem.name);
-            formData.append(
-                `menuItems[${index}][price]`,
-                (menuItem.price * 100).toString()
-            );
-        });
+    const menuItemsFormatted = restaurant.menuItems.map((item) => ({
+      ...item,
+      price: parseInt((item.price / 100).toFixed(2)),
+    }));
 
-        if (formDataJson.imageFile) {
-            formData.append(`imageFile`, formDataJson.imageFile);
-        }
-
-        onSave(formData);
+    const updatedRestaurant = {
+      ...restaurant,
+      deliveryPrice: deliveryPriceFormatted,
+      menuItems: menuItemsFormatted,
     };
 
-    return (
-        <Form {...form}>
-            {/* handleSubmit pasa las validaciones de zod, si todo esta bien se ejecuta el onSubmit */}
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
-                <DetailsSection />
-                <Separator />
-                <CuisinesSection />
-                <Separator />
-                <MenuSection />
-                <Separator />
-                <ImageSection />
+    form.reset(updatedRestaurant);
+  }, [form, restaurant]);
 
-                {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    const formData = new FormData();
 
-            </form>
-        </Form>
-    )
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
 
-}
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+
+    if (formDataJson.imageFile) {
+      formData.append(`imageFile`, formDataJson.imageFile);
+    }
+
+    onSave(formData);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+      </form>
+    </Form>
+  );
+};
 
 export default ManageRestaurantForm;
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,10 +1,13 @@
-import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useGetMyRestaurant, useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
     const { createRestaurant, isLoading } = useCreateMyRestaurant();
+
+    const { restaurant } = useGetMyRestaurant();
+
     return (
-        <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading}/>
+        <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
     );
 }
 
