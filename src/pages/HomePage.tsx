1075d2611151643202eb757f02ac5d508293d2bd
franciscoryb1 <franciscoryb1@hotmail.com>
2025-02-12 21:56:16 -0300

    ADD SerarchBar.tsx en HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,46 @@
 import landingImage from "../assets/landing.png"
 import appDownloadImage from "../assets/appDownload.png"
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return(
-    <div className="flex flex-col gapt-12">
 
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16"> {/*Margen negativo lleva el elemento hacia arriba */}
-            <h1 className="text-5xl font-bold tracking-thigt text-orange-600">
-                Tuck into a takeaway today
-            </h1>
-            <span className="text-xl">Food is just a click away!</span>
-        </div>
+    const navigate = useNavigate();
+    const handleSearchSubmit = (searchFormValues: SearchForm) => {
+        navigate({
+            pathname: `/search/${searchFormValues.searchQuery}`,
+        })
+    };
 
-        <div className="grid md:grid-cols-2 gap-5">
-            <img src={landingImage} />
-            <div className="flex flex-col items-center justify-center gap-4 text-center">
-                <span className="font-bold text-3xl tracking-tighter">
-                    Order takeaway even faster!
-                </span>
-                <span>
-                    Download the MerEats App for faster ordening and personalized recomdations.
-                </span>
-                <img src={appDownloadImage} />
+    return (
+        <div className="flex flex-col gapt-12">
+
+            <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                    Tuck into a takeway today
+                </h1>
+                <span className="text-xl">Food is just a click away!</span>
+                <SearchBar
+                    placeHolder="Search by City or Town"
+                    onSubmit={handleSearchSubmit}
+                />
             </div>
-        </div>
 
-    </div>
-  )
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage} />
+                <div className="flex flex-col items-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3xl tracking-tighter">
+                        Order takeaway even faster!
+                    </span>
+                    <span>
+                        Download the MerEats App for faster ordening and personalized recomdations.
+                    </span>
+                    <img src={appDownloadImage} />
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default HomePage;
