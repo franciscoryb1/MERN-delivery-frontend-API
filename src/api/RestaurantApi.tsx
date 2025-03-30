df31e12e16aa7a57c9f54e7af74a9790544fce68
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-03-30 13:09:02 -0300

    ADD URLSearchParams en RestaurantApi-tsx

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,3 +1,4 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import e from "express";
 import { useQuery } from "react-query";
@@ -6,12 +7,16 @@ import { data } from "react-router-dom";
 // Todos los hooks y requests para comunicarse con el backend al endpoint del Restaurant
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
     // Hook para buscar restaurantes por ciudad
     // Se usa el hook useQuery de react-query para hacer la peticion al backend y obtener los restaurantes
     const createSearchrequest = async (): Promise<RestaurantSearchResponse> => {
+
+        const params = new URLSearchParams();
+        params.set("searchQuery", searchState.searchQuery); // Agrega la consulta de búsqueda a los parámetros de la URL
+
         const response = await fetch(
-            `${API_BASE_URL}/api/restaurant/search/${city}`
+            `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
         );
 
         if (!response) {
@@ -22,7 +27,7 @@ export const useSearchRestaurants = (city?: string) => {
     };
 
     const { data: results, isLoading } = useQuery(
-        ["searchRestaurants"],
+        ["searchRestaurants", searchState], // cada vez que cambie el searchState se vuelve a ejecutar la query
         createSearchrequest,
         {
             enabled: !!city, // Solo se ejecuta si city no es undefined
