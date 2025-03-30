76ab8f87d24c339075a54e84f17411a89e883bb0
franciscoryb1 <desarrollo2@renovasrl.com.ar>
2025-03-30 13:09:37 -0300

    ADD SearchState en SearchPage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,40 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+
+export type SearchState = {
+    searchQuery: string;
+}
+
 
 const SearchPage = () => {
     const { city } = useParams(); // city porque asi lo llame en AppRoutes.tsx al definir la ruta
-    const { results, isLoading } = useSearchRestaurants(city); // Hook para buscar restaurantes por ciudad
+
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: "", // Estado inicial de la búsqueda
+    })
+
+    const setSearchQuery = (searchFormData: SearchForm) => {
+        // Copia todo el estado anterior y actualiza solo la propiedad searchQuery
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: searchFormData.searchQuery, // Actualiza el estado con la nueva consulta de búsqueda
+        }))
+    }
+
+    const resetSearch = () => {
+        // Copia todo el estado anterior y actualiza solo la propiedad searchQuery a una cadena vacía
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: "",
+        }))
+    }
+
+    const { results, isLoading } = useSearchRestaurants(searchState, city); // Hook para buscar restaurantes por ciudad
 
     if (isLoading) {
         return <span>Loading...</span> // Muestra un mensaje de carga mientras se obtienen los resultados
@@ -24,6 +53,14 @@ const SearchPage = () => {
             </div>
 
             <div id="main-content" className="flex flex-col gap-5">
+
+                <SearchBar
+                    searchQuery={searchState.searchQuery} // a la busqueda le asigno el valor actual del estado, si se actualiza el estado se re renderiza el componente
+                    onSubmit={setSearchQuery}
+                    placeHolder="Search by Cuisine or Restaurant name"
+                    onReset={resetSearch}
+                />
+
                 <SearchResultInfo total={results?.pagination?.total ?? 0} city={city ?? "Unknown"}
                 />
                 {
