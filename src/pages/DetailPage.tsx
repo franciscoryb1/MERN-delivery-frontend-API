9c6b21a951c6cf400f04f301be8fd88c1a6ca32e
franciscoryb1 <franciscoryb1@hotmail.com>
2025-04-02 20:43:04 -0300

    FIX DetailPage.tsx add addToCart function

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,12 +1,54 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItemComponent from "@/components/MenuItemComponent";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+    _id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
 
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const addToCart = (menuItem: MenuItem) => {
+        setCartItems((prevCartItems) => {
+            // 1) Verificar si el item ya está en el carrito
+            const existingItem = prevCartItems.find(
+                (item) => item._id === menuItem._id
+            );
+
+            let updatedCartItems;
+
+            // 2) Si está, aumentar la cantidad
+            if (existingItem) {
+                updatedCartItems = prevCartItems.map((item) =>
+                    item._id === menuItem._id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                );
+            } else {
+                // 3) Si no está, agregarlo al carrito
+                updatedCartItems = [
+                    ...prevCartItems,
+                    { ...menuItem, quantity: 1 },
+                ];
+            }
+
+            return updatedCartItems;
+        });
+    };
 
     if (isLoading || !restaurant) {
         return "Loading...";
@@ -22,13 +64,22 @@ const DetailPage = () => {
             </AspectRatio>
 
             <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+
                 <div className="flex flex-col gap-4">
                     <RestaurantInfo restaurant={restaurant} />
                     <span className="text-2xl font-bold tracking-tight">Menu</span>
                     {restaurant.menuItems.map((menuItem) => (
-                        <MenuItemComponent key={menuItem._id} menuItem={menuItem} />
+                        <MenuItemComponent key={menuItem._id} menuItem={menuItem} addToCart={() => addToCart(menuItem)} />
                     ))}
                 </div>
+
+                <Card>
+                    <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+                </Card>
+                <div>
+
+                </div>
+
             </div>
 
         </div>
