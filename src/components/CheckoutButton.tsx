8654876b59b461dd2a46538cdcdb775876b64fd4
franciscoryb1 <franciscoryb1@hotmail.com>
2025-04-14 21:42:33 -0300

    ADD return with Dialog in CheckoutButton

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -2,11 +2,21 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import UserProfileForm, { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton = () => {
+type Props = {
+    onCheckout: (userFormData: UserFormData) => void;
+    disabled: boolean;
+};
+
+const CheckoutButton = ({ onCheckout, disabled }: Props) => {
     const { isAuthenticated, isLoading: isAuthLoading, loginWithRedirect } = useAuth0();
 
     const { pathname } = useLocation();
+
+    const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
 
     const onLogin = async () => {
         await loginWithRedirect({
@@ -23,9 +33,26 @@ const CheckoutButton = () => {
             </Button>);
     };
 
-    if(isAuthLoading) {
+    if (isAuthLoading || !currentUser) {
         return <LoadingButton />
     }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button disabled={disabled} className="bg-orange-500 flex-1">
+                    Go to Check out
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-500">
+                <UserProfileForm
+                    currentUser={currentUser}
+                    onSave={onCheckout}
+                    islLoading={isGetUserLoading}
+                />
+            </DialogContent>
+        </Dialog>
+    );
 
 };
 
