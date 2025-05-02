1c4c16174248921daf89a05e7cb595ffcd71b6e7
franciscoryb1 <franciscoryb1@hotmail.com>
2025-05-02 11:25:40 -0300

    ADD useUpdateMyRestaurantOrder hook, UseState for order status and handleStatusChange function in OrderItemCard component

--- src/components/OrderItemCard.tsx
+++ src/components/OrderItemCard.tsx
@@ -1,16 +1,25 @@
-import { Order } from "@/types";
+import { Order, OrderStatus } from "@/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Separator } from "./ui/separator";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { ORDER_STATUS } from "@/config/order-status-config";
+import { useUpdateMyRestaurantOrder } from "@/api/MyRestaurantApi";
+import { useState } from "react";
 
 type Props = {
     order: Order;
 }
 
 const OrderItemCard = ({ order }: Props) => {
+    const { updateRestaurantStatus, isLoading } = useUpdateMyRestaurantOrder();
+    const [status, setStatus] = useState<OrderStatus>(order.status as OrderStatus);
+
+    const handleStatusChange = async (newStatus: OrderStatus) => {
+        await updateRestaurantStatus({ orderId: order._id as string, status: newStatus });
+        setStatus(newStatus);
+    };
 
     const getTime = () => {
         const orderDateTime = new Date(order.createdAt);
@@ -53,32 +62,32 @@ const OrderItemCard = ({ order }: Props) => {
 
             <CardContent className="flex flex-col gap-6">
                 <div className="flex flex-col gap-2">
-                    {
-                        order.cartItems.map((cartItem) => (
-                            <span>
-                                <Badge variant="outline" className="mr2">
-                                    {cartItem.quantity}
-                                </Badge>
-                                {cartItem.name}
-                            </span>
-                        ))
-                    }
+                    {order.cartItems.map((cartItem, index) => (
+                        <span key={`${cartItem.name}-${index}`}>
+                            <Badge variant="outline" className="mr2">
+                                {cartItem.quantity}
+                            </Badge>
+                            {cartItem.name}
+                        </span>
+                    ))}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                     <Label htmlFor="status">What is the status of this order?</Label>
-                    <Select >
+                    <Select
+                        value={status}
+                        disabled={isLoading}
+                        onValueChange={handleStatusChange}
+                    >
                         <SelectTrigger id="status">
                             <SelectValue placeholder="Status" />
-                            <SelectContent position="popper">
-                                {
-                                    ORDER_STATUS.map((status) => (
-                                        <SelectItem value={status.value}>
-                                            {status.label}
-                                        </SelectItem>
-                                    ))
-                                }
-                            </SelectContent>
                         </SelectTrigger>
+                        <SelectContent position="popper">
+                            {ORDER_STATUS.map((statusOption) => (
+                                <SelectItem key={statusOption.value} value={statusOption.value}>
+                                    {statusOption.label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
                     </Select>
                 </div>
             </CardContent>
